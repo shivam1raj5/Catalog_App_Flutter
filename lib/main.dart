aeaa4f107193907164399184877b17e6e170424c
Shivam <shivam1raj5@gmail.com>
2022-11-05 23:02:50 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Container(
+            child: Text("Shivam"),
+          ),
+        ),
+      ),
+    );
+  }
+}
