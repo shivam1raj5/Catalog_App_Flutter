2a6123e749442f067cba2a9f6f0557f0055c6187
shivam1raj5 <shivam1raj5@gmail.com>
2023-01-08 01:30:32 +0530

    day17 complited with image and details added with designe

--- lib/Pages/Homepage.dart
+++ lib/Pages/Homepage.dart
@@ -38,21 +38,22 @@ class _homepageState extends State<homepage> {
 
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: MyTheme.creamColor,
         body: SafeArea(
-      child: Container(
-        padding: Vx.m32,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CatalogHeader(),
-            if (CatalogModel.items.isNotEmpty)
-              CatalogList().expand()
-            else
-              Center(child: CircularProgressIndicator())
-          ],
-        ),
-      ),
-    ));
+          child: Container(
+            padding: Vx.m32,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CatalogHeader(),
+                if (CatalogModel.items.isNotEmpty)
+                  CatalogList().expand()
+                else
+                  Center(child: CircularProgressIndicator())
+              ],
+            ),
+          ),
+        ));
   }
 }
 
@@ -97,11 +98,52 @@ class CatalogItem extends StatelessWidget {
     return VxBox(
       child: Row(
         children: [
-          Image.network(
-            catalog.imageurl,
-          )
+          CatalogImage(
+            imageurl: catalog.imageurl,
+            image: '',
+          ),
+          Expanded(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              catalog.name.text.lg.color(MyTheme.darkBluishColor).bold.make(),
+              catalog.desc.text.textStyle(context.captionStyle).make(),
+              10.heightBox,
+              ButtonBar(
+                alignment: MainAxisAlignment.spaceBetween,
+                buttonPadding: EdgeInsets.zero,
+                children: [
+                  "\$${catalog.price}".text.bold.xl.make(),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(MyTheme.darkBluishColor),
+                        shape: MaterialStateProperty.all(StadiumBorder())),
+                    child: "BUY".text.make(),
+                  )
+                ],
+              ).pOnly(right: 8.0)
+            ],
+          ))
         ],
       ),
-    ).white.square(100).make();
+    ).white.roundedLg.square(150).make().py16();
+  }
+}
+
+class CatalogImage extends StatelessWidget {
+  final String imageurl;
+  const CatalogImage({
+    Key? key,
+    required this.imageurl,
+    required String image,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(
+      imageurl,
+    ).box.rounded.p8.color(MyTheme.creamColor).make().p16().w40(context);
   }
 }
