e9e8fd9f77618aceb190910481856aace6e28d70
shivam1raj5 <shivam1raj5@gmail.com>
2023-01-09 22:12:09 +0530

    Detail page creade and Image and therer details added on that page page

--- lib/Pages/Home_detail_page.dart
+++ lib/Pages/Home_detail_page.dart
@@ -16,15 +16,50 @@ class HomeDetailPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       backgroundColor: MyTheme.creamColor,
+      bottomNavigationBar: Container(
+        color: Colors.white,
+        child: ButtonBar(
+          alignment: MainAxisAlignment.spaceBetween,
+          buttonPadding: EdgeInsets.zero,
+          children: [
+            "\$${catalog.price}".text.bold.xl4.red800.make(),
+            ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all(MyTheme.darkBluishColor),
+                  shape: MaterialStateProperty.all(StadiumBorder())),
+              child: "BUY".text.make(),
+            ).wh(100, 50)
+          ],
+        ).p32(),
+      ),
       body: SafeArea(
+        bottom: false,
         child: Column(children: [
           Hero(
                   tag: Key(catalog.id.toString()),
                   child: Image.network(catalog.imageurl))
               .p16(),
           Expanded(
-              child: Container(
-            color: Colors.red,
+              child: VxArc(
+            height: 30.0,
+            arcType: VxArcType.CONVEY,
+            edge: VxEdge.TOP,
+            child: Container(
+              color: Colors.white,
+              width: context.screenWidth,
+              child: Column(
+                children: [
+                  catalog.name.text.xl4
+                      .color(MyTheme.darkBluishColor)
+                      .bold
+                      .make(),
+                  catalog.desc.text.textStyle(context.captionStyle).xl.make(),
+                  10.heightBox,
+                ],
+              ).py64(),
+            ),
           ))
         ]),
       ),
