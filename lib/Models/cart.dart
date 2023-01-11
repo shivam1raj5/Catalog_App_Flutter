ad03cb9ff7fb8e280e5e096140ea20258ab26648
shivam1raj5 <shivam1raj5@gmail.com>
2023-01-12 00:48:59 +0530

    day23 complited and cart.dart file created and added all function to add or retrive data to cart.dart page

---
+++ lib/Models/cart.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Models/catalog.dart';
+
+class CartModel {
+  //catalog field
+  late CatalogModel _catalog;
+
+  //collection of ID's - Store ID's each item
+  final List<int> _itemIds = [];
+
+  //Get Catalog
+  CatalogModel get catalog => _catalog;
+
+  set Catalog(CatalogModel newCatalog) {
+    assert(newCatalog != null);
+    _catalog = newCatalog;
+  }
+
+  //Get item in the cart
+  List<Item> get items => _itemIds.map((id) => _catalog.getById(id)).toList();
+
+  //Get total price
+  num get totalPrice =>
+      items.fold(0, (total, current) => total + current.price);
+
+  //Add item
+  void add(Item item) {
+    _itemIds.add(item.id);
+  }
+
+  //Remove Item
+  void remove(Item item) {
+    _itemIds.remove(item.id);
+  }
+}
