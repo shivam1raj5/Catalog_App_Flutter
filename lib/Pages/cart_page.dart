2fe69bd400822e0cf9d41c4db217e5ae40150116
shivam1raj5 <shivam1raj5@gmail.com>
2023-01-14 00:50:08 +0530

    day28 complited + Removemutation added in cart_page.dart + To change button icon in Add_To_Cart page when remove the item from cart page , we add Addmutation and Removemutation both + To update price on Cart_page wrape parice code part with Vxconsumer

--- lib/Pages/cart_page.dart
+++ lib/Pages/cart_page.dart
@@ -35,11 +35,17 @@ class _CartTotal extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          "\$${_cart!.totalPrice}"
-              .text
-              .xl5
-              .color(context.theme.colorScheme.secondary)
-              .make(),
+          VxConsumer(
+            notifications: {},
+            mutations: {RemoveMutation},
+            builder: (context, _, __) {
+              return "\$${_cart!.totalPrice}"
+                  .text
+                  .xl5
+                  .color(context.theme.colorScheme.secondary)
+                  .make();
+            },
+          ),
           30.widthBox,
           ElevatedButton(
             onPressed: () {
@@ -62,6 +68,7 @@ class _CartTotal extends StatelessWidget {
 class _CartList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    VxState.watch(context, on: [RemoveMutation]);
     final CartModel? _cart = (VxState.store as MyStore).cart;
     return _cart!.items.isEmpty
         ? "Nothing to show".text.makeCentered()
@@ -71,10 +78,7 @@ class _CartList extends StatelessWidget {
               leading: Icon(Icons.done),
               trailing: IconButton(
                 icon: Icon(Icons.remove_circle_outline),
-                onPressed: () {
-                  _cart.remove(_cart.items[index]);
-                  //setState(() {});
-                },
+                onPressed: () => RemoveMutation(item: _cart.items[index]),
               ),
               title: _cart.items[index].name.text.make(),
             ),
